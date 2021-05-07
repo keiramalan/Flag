f21f9cd0d1da078d41cca5e651c4d97fc17b9a01
keira malan <keira.malan@student.onslow.school.nz>
2021-05-07 13:25:38 +1200

    Initial sharing of project

---
+++ Driver.java
@@ -0,0 +1,49 @@
+/* Based on the ecs 100 template
+ * Code for creating a flagstaff and allowing a user to bring it up and down
+ * Name: Keira Malan
+ * Date: 7/5/21
+ */
+
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+
+
+/** Create flagstaff with widgets to move up and down
+ */
+public class Driver{
+    // fields
+    // max height of flag
+    // min height of flag
+    
+    /**      */
+    public Driver(){
+    UI.initialise();
+    UI.addButton("Quit", UI::quit);
+    }
+
+    // methods
+    
+    // method for raising flag
+    // check flag position boundaries
+    
+    // method for lowering flag
+    // check flag position boundaries
+    
+    // method with slider to set move amount
+    
+    // text field with name of flag to print name
+
+    public static void main(String[] args){
+        Driver obj = new Driver();
+        // add buttons
+        // button to raise flag
+        // button to lower flag
+        // slider for how much to raise flag
+        
+    }
+
+}
+
---
+++ Flag.java
@@ -0,0 +1,30 @@
+/* Based on the ecs 100 template
+ * Code for ??
+ * Name: Keira Malan
+ * Date: 7/5/21
+ */
+
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+
+
+/** Suppport class to draw a flag
+ */
+public class Flag{
+
+    /**
+     * Constructor for objects of class Flag
+     */
+    // Draw flagstaff
+    public Flag() {
+        // fields for the position of flagstaff
+        // and height above base
+    }
+    
+    // redraw method to draw line for pole and rectangle for flag
+    
+}
+
